0b74031f68fd1bf0c7be64758b5f0d1ed8081011
bproorda <bproorda@gmail.com>
2020-02-18 08:11:37 -0600

    changed js to cloning function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 console.log("js is working");
 
+//part of newmethod
+var template = document.getElementById("template");
+
 function addNew() { 
     var newItem = document.getElementById('newitem');
     var newItemValue = document.getElementById('newitem').value;
@@ -7,17 +10,33 @@ function addNew() {
 
     if (newItemValue === "") {
     } else {
-    //myversion
-    var newLi = document.createElement("li");
-    var newText = document.createTextNode(newItemValue);
-    newLi.appendChild(newText);
+    //myversion -orignal method
+    // var newLi = document.createElement("li");
+    // var newText = document.createTextNode(newItemValue);
+    // newLi.appendChild(newText);
+    // var position = document.getElementById("list");
+    // position.appendChild(newLi);
+    // document.getElementById('newitem').value = "";
+    // var btn = document.createElement('button');
+    // btn.innerHTML = "close"
+    // position = document.getElementById("list")
+    // position.appendChild(btn);
+
+    //newmethod - clone template node, change text then append
+    var temp = template.cloneNode(true);
+    temp.style.visibility = "visible";
+    var txtNode = document.createTextNode(newItemValue);
+    var pos = temp.childNodes;
+    console.log(pos.length);
+    var x = pos[0];
+    console.log(x);
+    temp.replaceChild(txtNode, x);
+    x = pos[0];
+    console.log(x);
     var position = document.getElementById("list");
-    position.appendChild(newLi);
-    document.getElementById('newitem').value = "";
-    var btn = document.createElement('button');
-    btn.innerHTML = "close"
-    position = document.getElementById("list")
-    position.appendChild(btn);
+    position.appendChild(temp);
+    //need to change id, and get button to work
+
     
     
     
